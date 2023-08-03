a7c2c72e65e61e8c18e0286dea32214059a735d5
Chiqui Kate Orduna <chiquikaaate@gmail.com>
2023-08-03 22:57:42 +1000

    Added brewery map

--- brewery-finder/src/pages/BreweryDetails.jsx
+++ brewery-finder/src/pages/BreweryDetails.jsx
@@ -5,20 +5,47 @@ import {
   faPhone,
   faLaptop,
   faLeftLong,
+  faThumbsUp,
+  faHeart,
 } from "@fortawesome/free-solid-svg-icons";
 
 import { useParams, NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
-// import { useState, useCallback, useEffect } from "react";
-// import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 function BreweryDetails() {
   const [data, setData] = useState([]);
   const { id } = useParams();
 
-  console.log(data);
+  const [map, setMap] = useState(null);
+
+  const latlong = {
+    lat: parseFloat(data.latitude),
+    lng: parseFloat(data.longitude),
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCq71N1GHDQWEhBF16jWgKY-pN4nM0fUFM",
+  });
+
+  const onLoad = useCallback(
+    (map) => {
+      const bounds = new window.google.maps.LatLngBounds(latlong);
+      map.fitBounds(bounds);
+      setMap(map);
+    },
+    [setMap, latlong]
+  );
+
+  const onUnmount = useCallback(
+    (map) => {
+      setMap(null);
+    },
+    [setMap]
+  );
 
   useEffect(() => {
     axios
@@ -37,7 +64,10 @@ function BreweryDetails() {
           Back
         </div>
       </NavLink>
+
       <div className="indiv-details">
+        <FontAwesomeIcon icon={faThumbsUp} style={{ color: "#ffefcd" }} />
+        <FontAwesomeIcon icon={faHeart} style={{ color: "#ffefcd" }} />
         <p className="i-name">
           <strong>{data.name}</strong>
         </p>
@@ -62,6 +92,27 @@ function BreweryDetails() {
         <p className="brewery-type">
           <strong>Brewery Type:</strong> {data.brewery_type}
         </p>
+      </div>
+
+      <div className="google-maps-container">
+        <div className="google-maps">
+          {isLoaded ? (
+            data.latitude &&
+            data.longitude && (
+              <GoogleMap
+                mapContainerStyle={{ width: "100%", height: "100%" }}
+                center={latlong}
+                zoom={10}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+              >
+                <Marker position={latlong} />
+              </GoogleMap>
+            )
+          ) : (
+            <div>Google Map has an error.</div>
+          )}
+        </div>
       </div>
     </div>
   );
